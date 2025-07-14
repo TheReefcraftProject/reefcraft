3201a4b52ccc07d043bc3459f9bae8578f9399d0
Justin Ebert <justin.c.ebert@gmail.com>
2025-07-13 18:07:13 -0600

    Clean up mouse handling

--- src/reefcraft/views/ui.py
+++ src/reefcraft/views/ui.py
@@ -67,10 +67,6 @@ class Slider:
         self._bg_mesh.add_event_handler(self._on_pointer_move, "pointer_move")
         self._bg_mesh.add_event_handler(self._on_pointer_up, "pointer_up")
 
-        self._fg_mesh.add_event_handler(self._on_pointer_down, "pointer_down")
-        self._fg_mesh.add_event_handler(self._on_pointer_move, "pointer_move")
-        self._fg_mesh.add_event_handler(self._on_pointer_up, "pointer_up")
-
         self._update_visuals()
 
     # ------------------------------------------------------------------
@@ -112,15 +108,18 @@ class Slider:
     # Event handlers
     # ------------------------------------------------------------------
     def _on_pointer_down(self, event: gfx.PointerEvent) -> None:
+        """Capture the mouse for slider control."""
         self._dragging = True
         event.target.set_pointer_capture(event.pointer_id, self.panel.renderer)
         self._set_from_screen_x(event.x)
 
     def _on_pointer_move(self, event: gfx.PointerEvent) -> None:
+        """Update the dragging on the slider."""
         if self._dragging:
             self._set_from_screen_x(event.x)
 
     def _on_pointer_up(self, event: gfx.PointerEvent) -> None:
+        """Release the mouse on mouse up."""
         if self._dragging:
             self._dragging = False
             event.target.release_pointer_capture(event.pointer_id)
@@ -147,6 +146,11 @@ class Panel:
         mesh = gfx.Mesh(geom, mat)
         mesh.local.position = (-((1920 / 2) - (300 / 2)), 0, -100)
 
+        # Block the picking for the background of the panel
+        mesh.material.pick_write = True
+        mesh.add_event_handler(self._on_pointer_down, "pointer_down")
+        mesh.add_event_handler(self._on_pointer_up, "pointer_up")
+
         self.scene = gfx.Scene()
         self.camera = gfx.OrthographicCamera(width=1920, height=1080)
         self.scene.add(mesh)
@@ -154,6 +158,16 @@ class Panel:
         Slider(self, left=20, upper=20, width=250, height=20, min_value=0, max_value=100, value=10)
         Slider(self, left=20, upper=50, width=250, height=20, min_value=0, max_value=100, value=70)
         Slider(self, left=20, upper=80, width=250, height=20, min_value=0, max_value=100, value=15)
+
+    def _on_pointer_down(self, event: gfx.PointerEvent) -> None:
+        """When the mouse is clicked in background of the panel, capture the mouse and block others."""
+        event.target.set_pointer_capture(event.pointer_id, self.renderer)
+        pass
+
+    def _on_pointer_up(self, event: gfx.PointerEvent) -> None:
+        """Release the mouse on mouse up."""
+        event.target.set_pointer_capture(event.pointer_id, self.renderer)
+        pass
 
     def draw(self) -> None:
         """Draw a solid rectangle on the left side of the UI scene."""
